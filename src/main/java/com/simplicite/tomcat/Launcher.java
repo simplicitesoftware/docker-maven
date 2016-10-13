3e027123606f11b7be8d6310df98b913707e3027
David AZOULAY <dazoulay@simplicite.fr>
2016-10-13 22:14:18 +0200

    Indent

--- src/main/java/com/simplicite/tomcat/Launcher.java
+++ src/main/java/com/simplicite/tomcat/Launcher.java
@@ -6,54 +6,54 @@ import java.io.FileOutputStream;
 import org.apache.catalina.startup.Tomcat;
 
 public class Launcher {
-        private int port = 8080;
-        private String rootPath = "webapps/ROOT";
+	private int port = 8080;
+	private String rootPath = "webapps/ROOT";
 
-        public Launcher() throws Exception {
-        }
+	public Launcher() throws Exception {
+	}
 
-        public void launch() throws Exception {
-                Tomcat tomcat = new Tomcat();
+	public void launch() throws Exception {
+		Tomcat tomcat = new Tomcat();
 
-                File f = new File("").getAbsoluteFile();
-                tomcat.setBaseDir(f.getAbsolutePath());
-                tomcat.getServer().setCatalinaHome(f);
-                tomcat.getServer().setCatalinaBase(f);
-                System.out.println("--- Tomcat home and base dirs set to [" + tomcat.getServer().getCatalinaHome() + "]");
+		File f = new File("").getAbsoluteFile();
+		tomcat.setBaseDir(f.getAbsolutePath());
+		tomcat.getServer().setCatalinaHome(f);
+		tomcat.getServer().setCatalinaBase(f);
+		System.out.println("--- Tomcat home and base dirs set to [" + tomcat.getServer().getCatalinaHome() + "]");
 
-                tomcat.enableNaming();
+		tomcat.enableNaming();
 
-                tomcat.setPort(port);
-                System.out.println("--- Tomcat port set to [" + port + "]");
+		tomcat.setPort(port);
+		System.out.println("--- Tomcat port set to [" + port + "]");
 
-                File root= new File(rootPath);
-                String rootAbsPath = root.getAbsolutePath();
-                System.out.print("--- Looking for ROOT webapp in [" + rootAbsPath + "]... ");
-                if (!root.exists()) throw new Exception("Missiing ROOT webapp");
-                System.out.println("Done");
+		File root= new File(rootPath);
+		String rootAbsPath = root.getAbsolutePath();
+		System.out.print("--- Looking for ROOT webapp in [" + rootAbsPath + "]... ");
+		if (!root.exists()) throw new Exception("Missiing ROOT webapp");
+		System.out.println("Done");
 
-                System.out.print("--- Deploying ROOT webapp... ");
-                tomcat.addWebapp("", rootAbsPath);
-                System.out.println("Done");
+		System.out.print("--- Deploying ROOT webapp... ");
+		tomcat.addWebapp("", rootAbsPath);
+		System.out.println("Done");
 
-                tomcat.start();
+		tomcat.start();
 
-                Runtime.getRuntime().addShutdownHook(new Thread() {
-                        @Override
-                        public void run() {
-                                System.out.print("--- Closing... ");
-                                System.out.println("Done");
-                        }
-                });
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				System.out.print("--- Closing... ");
+				System.out.println("Done");
+			}
+		});
 
-                tomcat.getServer().await();
-        }
+		tomcat.getServer().await();
+	}
 
-        public static void main(String[] args) {
-                try {
-                        new Launcher().launch();
-                } catch (Exception e) {
-                        e.printStackTrace();
-                }
-        }
+	public static void main(String[] args) {
+		try {
+			new Launcher().launch();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
